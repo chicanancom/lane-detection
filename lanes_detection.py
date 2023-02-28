5e4aefec91afcb3bae1c2052ae3c55c3a05ad4ce
= <duuu2001@gmail.com>
2023-02-28 11:15:30 +0700

    -

--- lanes_detection.py
+++ lanes_detection.py
@@ -2,38 +2,6 @@ import cv2
 import numpy as np
 
 
-def make_points(image, line):
-    slope, intercept = line
-    y1 = int(image.shape[0])
-    y2 = int(y1 * 3 / 5)
-    x1 = int((y1 - intercept) / slope)
-    x2 = int((y2 - intercept) / slope)
-    return [[x1, y1, x2, y2]]
-
-
-def average_slope_intercept(image, lines):
-    left_fit = []
-    right_fit = []
-    if lines is None:
-        return None
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            fit = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            if slope < 0:
-                left_fit.append((slope, intercept))
-            else:
-                right_fit.append((slope, intercept))
-
-    left_fit_average = np.average(left_fit, axis=0)
-    right_fit_average = np.average(right_fit, axis=0)
-    left_line = make_points(image, left_fit_average)
-    right_line = make_points(image, right_fit_average)
-    averaged_lines = [left_line, right_line]
-    return averaged_lines
-
-
 def canny(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     kernel = 5
@@ -69,11 +37,11 @@ def region_of_interest(canny):
 cap = cv2.VideoCapture("test2.mp4")
 while (cap.isOpened()):
     _, frame = cap.read()
+    lane_image = np.copy(cap)
     canny_image = canny(frame)
     cropped_canny = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_canny, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-    averaged_lines = average_slope_intercept(frame, lines)
-    line_image = display_lines(frame, averaged_lines)
+    lines = cv2.HoughLinesP(cropped_canny, 2, np.pi / 180, 100, np.array([]), 40, 5)
+    line_image = display_lines(frame, lines)
     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     cv2.imshow("result", combo_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
